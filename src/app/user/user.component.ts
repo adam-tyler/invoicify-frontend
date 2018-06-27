a3544b4ec9f3c2d64c956e7ed2c9467f064ab34f
BZK2P9@ALLYIPOC.COM <BZK2P9@ALLYIPOC.COM>
2018-06-27 15:26:16 -0400

    user and userform

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material';
+
+import { DataService } from '../data.service'
+import { DeleteConfirmComponent } from '../delete-confirm/delete-confirm.component'
+import { fadeInAnimation } from '../animations/fade-in.animation';
+
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
+  animations: [fadeInAnimation],
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  errorMessage: string;
+  successMessage: string;
+  users: any[];
 
-  ngOnInit() {
+  constructor (private dataService: DataService, public dialog: MatDialog) {}
+
+  ngOnInit() { this.getUsers(); }
+
+  getUsers() {
+    this.dataService.getRecords("user")
+      .subscribe(
+        users => this.users = users,
+        error =>  this.errorMessage = <any>error);
   }
+
+ 
 
 }
